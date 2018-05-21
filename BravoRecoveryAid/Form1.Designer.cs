f0eee9b76c63d59e756f4fbe7740ffaed73b4e5b
dmaas15 <dmaas15@yahoo.com>
2018-05-21 10:52:44 +0800

    Crash

--- BravoRecoveryAid/Form1.Designer.cs
+++ BravoRecoveryAid/Form1.Designer.cs
@@ -29,9 +29,15 @@
         private void InitializeComponent()
         {
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnOpenLogFile = new System.Windows.Forms.Button();
+            this.txtOpenLogFile = new System.Windows.Forms.TextBox();
+            this.dgvPipetteLog = new System.Windows.Forms.DataGridView();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnLastProtocol = new System.Windows.Forms.Button();
+            this.txtLastProtocol = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.runsetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -42,13 +48,9 @@
             this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
-            this.runsetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.txtStatus = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPipetteLog)).BeginInit();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -57,29 +59,29 @@
             this.openFileDialog1.FileName = "openFileDialog1";
             this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
             // 
-            // button1
+            // btnOpenLogFile
             // 
-            this.button1.Location = new System.Drawing.Point(417, 126);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(138, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Open Log File";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnOpenLogFile.Location = new System.Drawing.Point(417, 126);
+            this.btnOpenLogFile.Name = "btnOpenLogFile";
+            this.btnOpenLogFile.Size = new System.Drawing.Size(138, 23);
+            this.btnOpenLogFile.TabIndex = 0;
+            this.btnOpenLogFile.Text = "Open Log File";
+            this.btnOpenLogFile.UseVisualStyleBackColor = true;
+            this.btnOpenLogFile.Click += new System.EventHandler(this.button1_Click);
             // 
-            // textBox1
+            // txtOpenLogFile
             // 
-            this.textBox1.Location = new System.Drawing.Point(168, 126);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(243, 20);
-            this.textBox1.TabIndex = 1;
+            this.txtOpenLogFile.Location = new System.Drawing.Point(168, 126);
+            this.txtOpenLogFile.Name = "txtOpenLogFile";
+            this.txtOpenLogFile.Size = new System.Drawing.Size(243, 20);
+            this.txtOpenLogFile.TabIndex = 1;
             // 
-            // dataGridView1
+            // dgvPipetteLog
             // 
-            this.dataGridView1.AllowUserToAddRows = false;
-            this.dataGridView1.AllowUserToDeleteRows = false;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dgvPipetteLog.AllowUserToAddRows = false;
+            this.dgvPipetteLog.AllowUserToDeleteRows = false;
+            this.dgvPipetteLog.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPipetteLog.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Column1,
             this.Column2,
             this.Column3,
@@ -90,109 +92,39 @@
             this.Column8,
             this.Column9,
             this.Column10});
-            this.dataGridView1.Location = new System.Drawing.Point(12, 227);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.Size = new System.Drawing.Size(1310, 257);
-            this.dataGridView1.TabIndex = 2;
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dgvPipetteLog.Location = new System.Drawing.Point(12, 295);
+            this.dgvPipetteLog.Name = "dgvPipetteLog";
+            this.dgvPipetteLog.ReadOnly = true;
+            this.dgvPipetteLog.Size = new System.Drawing.Size(1310, 257);
+            this.dgvPipetteLog.TabIndex = 2;
+            this.dgvPipetteLog.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
-            // Column1
+            // btnClear
             // 
-            this.Column1.HeaderText = "Timestamp";
-            this.Column1.Name = "Column1";
-            this.Column1.ReadOnly = true;
-            this.Column1.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.btnClear.Location = new System.Drawing.Point(417, 156);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(138, 23);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.button2_Click);
             // 
-            // Column2
+            // btnLastProtocol
             // 
-            this.Column2.HeaderText = "Class";
-            this.Column2.Name = "Column2";
-            this.Column2.ReadOnly = true;
-            this.Column2.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.btnLastProtocol.Location = new System.Drawing.Point(597, 125);
+            this.btnLastProtocol.Name = "btnLastProtocol";
+            this.btnLastProtocol.Size = new System.Drawing.Size(146, 23);
+            this.btnLastProtocol.TabIndex = 4;
+            this.btnLastProtocol.Text = "Last Protocol";
+            this.btnLastProtocol.UseVisualStyleBackColor = true;
+            this.btnLastProtocol.Click += new System.EventHandler(this.button3_Click);
             // 
-            // Column3
+            // txtLastProtocol
             // 
-            this.Column3.HeaderText = "Device";
-            this.Column3.Name = "Column3";
-            this.Column3.ReadOnly = true;
-            this.Column3.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
-            // 
-            // Column4
-            // 
-            this.Column4.HeaderText = "Location";
-            this.Column4.Name = "Column4";
-            this.Column4.ReadOnly = true;
-            this.Column4.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
-            // 
-            // Column5
-            // 
-            this.Column5.HeaderText = "Process";
-            this.Column5.Name = "Column5";
-            this.Column5.ReadOnly = true;
-            this.Column5.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
-            // 
-            // Column6
-            // 
-            this.Column6.HeaderText = "Task";
-            this.Column6.Name = "Column6";
-            this.Column6.ReadOnly = true;
-            this.Column6.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
-            // 
-            // Column7
-            // 
-            this.Column7.HeaderText = "Description";
-            this.Column7.Name = "Column7";
-            this.Column7.ReadOnly = true;
-            this.Column7.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
-            // 
-            // Column8
-            // 
-            this.Column8.HeaderText = "Protocol Name";
-            this.Column8.Name = "Column8";
-            this.Column8.ReadOnly = true;
-            this.Column8.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
-            // 
-            // Column9
-            // 
-            this.Column9.HeaderText = "File Name";
-            this.Column9.Name = "Column9";
-            this.Column9.ReadOnly = true;
-            this.Column9.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
-            // 
-            // Column10
-            // 
-            this.Column10.HeaderText = "Session ID";
-            this.Column10.Name = "Column10";
-            this.Column10.ReadOnly = true;
-            this.Column10.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(417, 156);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(138, 23);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Clear";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(597, 125);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(146, 23);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "Last Protocol";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(749, 128);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(464, 20);
-            this.textBox2.TabIndex = 5;
+            this.txtLastProtocol.Location = new System.Drawing.Point(749, 128);
+            this.txtLastProtocol.Name = "txtLastProtocol";
+            this.txtLastProtocol.Size = new System.Drawing.Size(464, 20);
+            this.txtLastProtocol.TabIndex = 5;
             // 
             // menuStrip1
             // 
@@ -215,26 +147,116 @@
             // runsetToolStripMenuItem
             // 
             this.runsetToolStripMenuItem.Name = "runsetToolStripMenuItem";
-            this.runsetToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.runsetToolStripMenuItem.Size = new System.Drawing.Size(110, 22);
             this.runsetToolStripMenuItem.Text = "Runset";
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Timestamp";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Class";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Session ID";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Volume";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            this.Column4.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Asp Location";
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            this.Column5.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Asp Selection";
+            this.Column6.Name = "Column6";
+            this.Column6.ReadOnly = true;
+            this.Column6.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "Disp Location";
+            this.Column7.Name = "Column7";
+            this.Column7.ReadOnly = true;
+            this.Column7.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "Disp Selection";
+            this.Column8.Name = "Column8";
+            this.Column8.ReadOnly = true;
+            this.Column8.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Column9
+            // 
+            this.Column9.HeaderText = "Description";
+            this.Column9.Name = "Column9";
+            this.Column9.ReadOnly = true;
+            this.Column9.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Column10
+            // 
+            this.Column10.HeaderText = "File Name";
+            this.Column10.Name = "Column10";
+            this.Column10.ReadOnly = true;
+            this.Column10.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.Location = new System.Drawing.Point(749, 155);
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.Size = new System.Drawing.Size(464, 20);
+            this.txtStatus.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(683, 156);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Status:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1343, 645);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.txtLastProtocol);
+            this.Controls.Add(this.btnLastProtocol);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.dgvPipetteLog);
+            this.Controls.Add(this.txtOpenLogFile);
+            this.Controls.Add(this.btnOpenLogFile);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPipetteLog)).EndInit();
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
@@ -245,12 +267,15 @@
         #endregion
 
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnOpenLogFile;
+        private System.Windows.Forms.TextBox txtOpenLogFile;
+        private System.Windows.Forms.DataGridView dgvPipetteLog;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnLastProtocol;
+        private System.Windows.Forms.TextBox txtLastProtocol;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem runsetToolStripMenuItem;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
@@ -261,9 +286,8 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column10;
-        private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
-        private System.Windows.Forms.ToolStripMenuItem runsetToolStripMenuItem;
+        private System.Windows.Forms.TextBox txtStatus;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- BravoRecoveryAid/Form1.cs
+++ BravoRecoveryAid/Form1.cs
@@ -28,16 +28,16 @@ namespace BravoRecoveryAid
             OpenFileDialog openFileDialog1 = new OpenFileDialog();
             openFileDialog1.Title = "Browse Log Files";
             openFileDialog1.InitialDirectory = @"C:\VWorks Workspace\VWorks\Logs";
-            openFileDialog1.Filter = "log files (*.log)|*.log";
+            openFileDialog1.Filter = "log files (*.log)|*pipette*.log";
             openFileDialog1.CheckFileExists = true;
             openFileDialog1.CheckPathExists = true;
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 // Read each line of the file into a string array. Each element
                 // of the array is one line of the file.
-                textBox1.Text = openFileDialog1.FileName;
+                txtOpenLogFile.Text = openFileDialog1.FileName;
                 openFileDialog1.Dispose();
-                string fname = textBox1.Text;
+                string fname = txtOpenLogFile.Text;
                 string[] lines = System.IO.File.ReadAllLines(@fname);
 
                 // Display the file contents by using a foreach loop.
@@ -45,7 +45,7 @@ namespace BravoRecoveryAid
                 foreach (string line in lines)
                 {
                     string[] subStrings = line.Split('\t');
-                    dataGridView1.Rows.Add(subStrings);
+                    dgvPipetteLog.Rows.Add(subStrings);
                 }
 
             }
@@ -65,28 +65,35 @@ namespace BravoRecoveryAid
 
         private void button2_Click(object sender, EventArgs e)
         {
-            foreach (DataGridViewRow row in dataGridView1.SelectedRows)
+            foreach (DataGridViewRow row in dgvPipetteLog.SelectedRows)
             {
                 if (!row.IsNewRow)
-                    dataGridView1.Rows.Remove(row);
+                    dgvPipetteLog.Rows.Remove(row);
             }
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            int lastRow = dataGridView1.Rows.Count;
+            int lastRow = dgvPipetteLog.Rows.Count;
             int rowCount = 1;
+            int logPosition = 0;
             object protocol;
             //Console.WriteLine(lastRow);
             do
             {
-                protocol = dataGridView1.Rows[lastRow - rowCount].Cells[7].Value;
+                protocol = dgvPipetteLog.Rows[lastRow - rowCount].Cells[9].Value;
                 if (protocol!=null)
                 {
                     if (protocol.ToString() != "")
                     {
                         Console.WriteLine(protocol.GetType() + " " + protocol);
-                        textBox2.Text = protocol.ToString();
+                        txtLastProtocol.Text = protocol.ToString();
+                        logPosition = lastRow - rowCount;
+                        txtStatus.Text = dgvPipetteLog.Rows[logPosition].Cells[8].Value.ToString();
+                        if(txtStatus.TextLength==0)
+                        {
+                            LastTransfer(logPosition);
+                        }
                         break;
                     }
                 }
@@ -94,6 +101,11 @@ namespace BravoRecoveryAid
                 rowCount++;
             } while (rowCount<lastRow);
 ;
+        }
+
+        private void LastTransfer(int Position)
+        {
+            
         }
     }
 }
